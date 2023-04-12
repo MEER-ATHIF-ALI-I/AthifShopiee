42f39a3f8c8ba13c2c72eeda1574004ce835441e
Meer-Athif-Ali-I <meerathifali@gmail.com>
2023-04-12 23:44:15 +0530

    scripts removed

--- backend/server.js
+++ backend/server.js
@@ -11,24 +11,7 @@ app.use('/api/products/' , productsRoute)
 app.use('/api/users/',userRoute)
 app.use('/api/orders/',orderRoute)
 
-//----------deployment-----------------
 
-// __dirname = path.resolve();
-
-// if(process.env.NODE_ENV==="production") {
-//  app.use(express.static(path.join(__dirname,"/frontend/build")));
-
-//  app.get('*',(req,res)=>{
-//    res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
-//  });
-// }else{
-// 	app.get("/", (req, res)=>{
-//       res.send("API is running..");
-// 	});
-// }
-
-
-//----------deployment-----------
    
 
 const port = process.env.PORT || 8000;
